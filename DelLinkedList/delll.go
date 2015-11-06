334ff1439d932dbbd7c32c7652ad5fbcf41c1650
Gunjan Patel <patelgunjan5@gmail.com>
2015-11-05 17:22:29 -0800

    LRU /w linked list checkpoint 1

--- DelLinkedList/delll.go
+++ DelLinkedList/delll.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+type List struct {
+	head, tail *Node
+}
+
+func main() {
+	n1 := Node(4)
+	head = &n1
+	n2 := Node(6)
+	n1.next = n2
+	tail = null
+	n2.next = tail
+
+}
--- LRU/lru_map_linkedlist.go
+++ LRU/lru_map_linkedlist.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+var (
+	SIZE  = 5
+	cache = make(map[int]node)
+)
+
+type node struct {
+	val  int
+	next *node
+	prev *node
+}
+
+func main() {
+	input := [...]int{}
+
+}
